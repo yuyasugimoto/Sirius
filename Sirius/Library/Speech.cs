3bba56e480946bacab2166c96512a5a6139ac170
yuya-sugimoto <yuyataitoku7728@gmail.com>
2019-02-17 22:48:44 +0900

    init

---
+++ Sirius/Library/Speech.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.TextToSpeech.V1;
+
+namespace Sirius.Library
+{
+    class Speech
+    {
+
+        public void TextSpeech(string text)
+        {
+
+            TextToSpeechClient client = TextToSpeechClient.Create();
+
+            SynthesisInput input = new SynthesisInput
+            {
+                Text = text
+            };
+
+            VoiceSelectionParams voice = new VoiceSelectionParams
+            {
+                LanguageCode = "ja-JP",
+                SsmlGender = SsmlVoiceGender.Female
+            };
+
+            AudioConfig config = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Linear16,
+
+            };
+
+            var response = client.SynthesizeSpeech(
+                input, 
+                voice,
+                config
+            );
+
+            using (var memoryStream = new MemoryStream(response.AudioContent, true))
+            {
+                var player = new System.Media.SoundPlayer(memoryStream);
+                Console.Write("Play");
+                player.Play();
+            }
+
+        }
+
+    }
+}
---
+++ Sirius/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sirius.Library;
+
+namespace Sirius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cls_sp = new Sirius.Library.Speech();
+            Console.WriteLine("Hello");
+            cls_sp.TextSpeech("HELLO");
+            
+
+        }
+    }
+}
